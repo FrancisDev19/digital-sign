4f54e7174dd31230d5a7b60f201d136c9413a212
Francis Castillo <franciscastillocruz@gmail.com>
2022-07-08 17:24:20 -0400

    Something

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,23 +1,11 @@
 import Hero from "../components/Hero/Hero";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
-import { hasCookie } from "cookies-next";
 
-export const getServerSideProps = async (ctx) => {
-  const isAuthenticated = hasCookie("session");
-  console.log(isAuthenticated)
-
-  return {
-    props: {
-      isAuthenticated,
-    },
-  };
-};
-
-export default function Home({ isAuthenticated }) {
+export default function Home() {
   return (
     <>
-      <Navbar isAuthenticated={isAuthenticated} />
+      <Navbar />
       <Hero />
       <Footer />
     </>
